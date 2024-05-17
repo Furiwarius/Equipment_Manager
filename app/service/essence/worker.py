04b1ec5842e2f66ca0160e592f83b4d054ceff5d
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-05-17 09:48:15 +0300

    fix: Worker class changed for database

--- app/service/essence/worker.py
+++ app/service/essence/worker.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from dataclasses import dataclass
 import enum
 
-class WorkerTool(enum.Enum):
+class StatusWorker(enum.Enum):
     '''
     Статус работника
     '''
@@ -15,98 +16,80 @@ class WorkerTool(enum.Enum):
     sick = False
     
 
-
+@dataclass
 class Worker():
     '''
     Работник
     '''
 
-    def __init__(self, name:str, surname:str) -> None:
-        
-        # Имя
-        self.__name = name
-        # Фамилия
-        self.__surname =surname
-        # Номер телефона
-        self.__phone_number = None
-        # Должность работника
-        self.__job_title = None
-        # Дата начала работы на объекте
-        self.__start_date_work = datetime.now()
-        # Статус работника
-        self.__status = WorkerTool.works
-        # Объект на котором находится ответственное лицо
-        self.__construction = None
+    id: int
+    # Имя
+    name: str
+    # Фамилия
+    surname: str
+    # Номер телефона
+    phone_number: int
+    # Должность работника
+    job_title: str
+    # Дата начала работы на объекте
+    start_work: datetime
+    # Статус работника
+    status: StatusWorker
 
 
     def __nonzero__(self) -> bool:
 
-        return self.__status
+        return self.status
     
 
     def __str__(self) -> str:
 
-        if self.__job_title:
-            return f"{self.__name} {self.__surname} {self.__job_title}"
+        if self.job_title:
+            return f"{self.name} {self.surname} {self.job_title}"
         
-        return f"{self.__name}{self.__surname}"
+        return f"{self.name}{self.surname}"
         
     
-    def get_construction(self):
+    def get_id(self) -> int:
         '''
-        Получение объекта, на котором находится работник
+        Получение id
         '''
-        return self.__construction
+        return self.id
 
 
     def get_phone_number(self) -> str:
         '''
         Получение номера работника
         '''
-        return self.__phone_number
+        return self.phone_number
     
 
-    def get_start_date_work(self) -> datetime:
+    def get_date_work(self) -> datetime:
         '''
         Получение даты начала работы на объекте
         '''
-        return self.__start_date_work
+        return self.start_work
 
     
-    def change_job_title(self, new_job_title:str) -> None:
+    def change_title(self, new_job_title:str) -> None:
         '''
         Добавление должности
         '''
-        self.__job_title = new_job_title
+        self.job_title = new_job_title
 
     
-    def change_phone_number(self, new_number:str) -> None:
+    def change_phone(self, new_number:str) -> None:
         '''
         Изменение номера телефона
         '''
-        self.__phone_number = new_number
-
-
-    def change_construction(self, new_construction) -> None:
-        '''
-        Измененние объекта,
-        на котором находится работник
-        '''
-        self.__construction = new_construction
+        self.phone_number = new_number
     
 
-    def delete_construction(self) -> None:
-        '''
-        Удаление объекта строительства
-        '''
-        self.__construction = None
-
-
-    def change_start_date_work(self, new_date:datetime) -> None:
+    def change_date_work(self, new_date:datetime) -> None:
         '''
         Изменение даты начала работы на объекте
         '''
-        self.__start_date_work = new_date
+        self.start_work = new_date
 
 
     def change_status(self) -> None:
