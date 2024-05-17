02f7a4f66ab9053e7c17db2eb0ae6e26e25e8bbe
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-05-17 09:39:40 +0300

    fix: Tool class changed for database
    The Tool class is modified as if it were a row from a table in the database

--- app/service/essence/tool.py
+++ app/service/essence/tool.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dataclasses import dataclass
 import enum
 
 class ToolStatus(enum.Enum):
@@ -11,77 +12,49 @@ class ToolStatus(enum.Enum):
     faulty = False
 
 
+@dataclass
 class Tool():
     '''
     Инструмент
-
-    Для обхода ошибки взаимного импорта описание возвращаемого объекта
-    не указано у полейи __construction.
-    При создании экземпляра класса Tool эти поля заполняются 
-    методами get_construction
-    Возвращаемые значение у этих методов также не указаны.
     '''
-    
-    def __init__(self, name:str) -> None:
-        # Название инструмента
-        self.__name = name
-        # дата перемещения
-        self.__import_date = datetime.now()
-        self.__status = ToolStatus.works
 
-        # для обхода ошибки взаимного импорта описание возвращаемого объекта не указано
-        # при создании экземпляра класса Tool, 
-        # эти поля заполняются методами get_construction
+    id: int
+    name: str
+    import_date: datetime
+    status: ToolStatus
 
-        # Объект на котором находится инструмент
-        self.__construction = None
 
-    
     def __nonzero__(self) -> bool:
-        return self.__status
+        return self.status
     
 
-    def __str__(self) -> str:
-        return self.__name
+    def get_id(self) -> int:
+        return self.id
 
-    
-    # для обхода ошибки взаимного импорта описание возвращаемого объекта не указано
-    # возвращаемыый класс Сonstruction
-    def get_construction(self):
-
-        return self.__construction
-    
 
     def get_import_date(self) -> datetime:
-        return self.__import_date
+        return self.import_date
 
 
     # ВАЖНО! Так как, валидация данных происходит в другом классе,
     # в методах изменения она отсутствует
 
-    def change_import_date(self, new_date:datetime) -> None:
+    def change_date(self, new_date:datetime) -> None:
         '''
         Измененние даты перемещения инструмента
         '''
-        self.__import_date = new_date
+        self.import_date = new_date
 
 
-    def change_construction(self, new_construction) -> None:
-        '''
-        Изменение объекта, на котором находится инструмент
-        '''
-        self.__construction = new_construction
-
-
-    def break_tool(self):
+    def break_tool(self) -> None:
         '''
         Сломать инструмент
         '''
-        self.__status = ToolStatus.faulty
+        self.status = ToolStatus.faulty
     
 
-    def fix_tool(self):
+    def fix_tool(self) -> None:
         '''
         Починить инструмент
         '''
-        self.__status = ToolStatus.works
+        self.status = ToolStatus.works
