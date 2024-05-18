05aefdcc2fcd36c6581fe42846ff41cdd711a580
Vasiliy <110427738+Furiwarius@users.noreply.github.com>
2024-05-18 16:31:59 +0200

    fix: the Warehouse class has been modified

--- app/service/essence/fields/warehouse.py
+++ app/service/essence/fields/warehouse.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from tool import Tool
 
 @dataclass
 class Storage():
@@ -10,3 +10,27 @@ class Storage():
     id: int
     # название склада
     name: str
+
+    # список инструментов
+    tools: list
+
+
+    def get_tools(self) -> list:
+        '''
+        Получить список инструментов со склада
+        '''
+        return self.tools
+
+
+    def add_tool(self, tool: Tool) -> None:
+        '''
+        Добавить инструмент на склад
+        '''
+        self.tools.append(tool)
+    
+
+    def delete_tool(self, tool: Tool) -> None:
+        '''
+        Удалить инструмент со склада
+        '''
+        self.tools.remove(tool)
