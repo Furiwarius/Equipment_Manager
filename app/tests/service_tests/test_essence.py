a951a25005576f5310f2c37e785937e01a49613a
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-05-24 15:18:10 +0300

    feat: added a test for the method of adding responsible with construction to a new construction

--- app/tests/service_tests/test_essence.py
+++ app/tests/service_tests/test_essence.py
@@ -9,7 +9,7 @@ class TestSK():
     '''
 
     generator = DataGenerator()
-    storekeeper = Storekeeper(user_id=1)
+    stock = Storekeeper(user_id=1)
 
 
     def test_add_storage(self):
@@ -17,9 +17,9 @@ class TestSK():
         Тестрирование метода по добавлению склада
         '''
         storage = TestSK.generator.storage_generator()
-        TestSK.storekeeper.add_storage(storage=storage)
+        TestSK.stock.add_storage(storage=storage)
 
-        assert storage.get_id() in TestSK.storekeeper.storages.keys()
+        assert storage.get_id() in TestSK.stock.storages.keys()
     
     
     def test_add_tool(self):
@@ -27,12 +27,12 @@ class TestSK():
         Тестирование метода по добавлению инструмента
         '''
         storage = TestSK.generator.storage_generator()
-        TestSK.storekeeper.add_storage(storage=storage)
+        TestSK.stock.add_storage(storage=storage)
         
         tool = TestSK.generator.tool_generator()
-        TestSK.storekeeper.add_tool(tool=tool, storage=storage)
+        TestSK.stock.add_tool(tool=tool, storage=storage)
         assert tool.get_construction()==storage.get_id()
-        assert tool.get_id() in storage.get_tools() and tool.get_id() in TestSK.storekeeper.get_id_tools()
+        assert tool.get_id() in storage.get_tools() and tool.get_id() in TestSK.stock.get_id_tools()
 
     
     def test_add_worker(self):
@@ -40,9 +40,9 @@ class TestSK():
         Тестирование метода по добавлению работника
         '''
         worker = TestSK.generator.worker_generator()
-        TestSK.storekeeper.add_worker(worker=worker)
+        TestSK.stock.add_worker(worker=worker)
         
-        assert worker.get_id() in TestSK.storekeeper.get_id_workers()
+        assert worker.get_id() in TestSK.stock.get_id_workers()
     
 
     def test_add_construction(self):
@@ -50,19 +50,19 @@ class TestSK():
         Тестирование метода по добавлению объекта строительства
         '''
         construction = TestSK.generator.constr_generator()
-        TestSK.storekeeper.add_construction(construction=construction)
+        TestSK.stock.add_construction(construction=construction)
 
-        assert construction.get_id() in TestSK.storekeeper.get_id_construction()
+        assert construction.get_id() in TestSK.stock.get_id_construction()
     
 
     def test_appointment_healthy_responsible(self):
         '''
         Тестирование метода по назначению ответственного лица на объект (Работник здоров)
         '''
-        construction = TestSK.storekeeper.get_construction_by_id(TestSK.storekeeper.get_id_construction()[0])
-        worker = TestSK.storekeeper.get_worker_by_id(TestSK.storekeeper.get_id_workers()[0])
+        construction = TestSK.stock.get_construction_by_id(TestSK.stock.get_id_construction()[0])
+        worker = TestSK.stock.get_worker_by_id(TestSK.stock.get_id_workers()[0])
         
-        TestSK.storekeeper.appointment_responsible(worker=worker, construction=construction)
+        TestSK.stock.appointment_responsible(worker=worker, construction=construction)
 
         assert worker.get_id()==construction.get_worker() and worker.get_construction()==construction.get_id()
     
@@ -73,22 +73,38 @@ class TestSK():
         '''
         sick_worker = TestSK.generator.worker_generator()
         sick_worker.get_sick()
-        construction = TestSK.storekeeper.get_construction_by_id(TestSK.storekeeper.get_id_construction()[0])
+        construction = TestSK.stock.get_construction_by_id(TestSK.stock.get_id_construction()[0])
         
-        TestSK.storekeeper.appointment_responsible(worker=sick_worker, construction=construction)
+        TestSK.stock.appointment_responsible(worker=sick_worker, construction=construction)
 
         assert sick_worker.get_id()!=construction.get_worker()
         assert sick_worker.get_construction()!=construction.get_id()
+
+    
+    def test_appointment_engaged_responsible(self):
+        '''
+        Тестирование метода по назначению на объект ответственного 
+        лица с уже имеющимся объектом и инструментами на нем
+        '''
+        construction = TestSK.stock.get_construction_by_id(TestSK.stock.get_id_construction()[0])
+        worker = TestSK.stock.get_worker_by_id(construction.get_worker())
+
+        new_constr = TestSK.generator.constr_generator()
+        TestSK.stock.appointment_responsible(worker=worker,construction=new_constr)
+
+        assert construction.get_worker()==worker.get_id()
+        assert worker.get_construction()==construction.get_id()
+        assert new_constr.get_worker()!=worker.get_id()
     
 
     def test_move_tool(self):
         '''
         Тестирование метода по перемещению инструмента
         '''
-        tool = TestSK.storekeeper.get_tool_by_id(TestSK.storekeeper.get_id_tools()[0])
-        construction = TestSK.storekeeper.get_construction_by_id(TestSK.storekeeper.get_id_construction()[0])
+        tool = TestSK.stock.get_tool_by_id(TestSK.stock.get_id_tools()[0])
+        construction = TestSK.stock.get_construction_by_id(TestSK.stock.get_id_construction()[0])
 
-        TestSK.storekeeper.move_tool(tool=tool, where=construction)
+        TestSK.stock.move_tool(tool=tool, where=construction)
 
         assert tool.get_construction()==construction.get_id()
         assert tool.get_id() in construction.get_tools()
