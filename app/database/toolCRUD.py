da1ae6c082777a3e8c2d27c905ca634a752cf3c5
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-06-02 18:34:03 +0300

    feat: added a class for CRUD operations with tool

---
+++ app/database/toolCRUD.py
@@ -0,0 +1,35 @@
+from app.entities.tool import Tool
+from app.entities.worker import Worker
+from app.entities.construction import Construction
+from app.entities.storage import Storage
+
+
+class ToolCRUD():
+    '''
+    Класс для взаимодействия с БД
+    '''
+
+
+    def move_to_storage(self, tool:Tool, where:Storage) -> None:
+        '''
+        Перевезти инструмент на другой склад
+        '''
+    
+
+    def move_to_construction(self, tool:Tool, where:Construction) -> None:
+        '''
+        Перевезти инструмент на другой объект
+        '''
+    
+
+    def break_tool(self, tool:Tool) -> None:
+        '''
+        Сломать инструмент
+        '''
+    
+
+    def fix_tool(self, tool:Tool) -> None:
+        '''
+        Починить инструмент
+        '''
+    
