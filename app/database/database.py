6757d58cb2285b55183709ec5c5e3904615e9f94
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-06-08 12:30:31 +0300

    fix: methods for creating a database and creating a SQLalchemy engine have been moved to the Database class

--- app/database/database.py
+++ app/database/database.py
@@ -1,35 +1,53 @@
 from sqlalchemy import create_engine, Engine
 from mysql.connector import connect, Error
 from app.settings.settings import DATABASE_PASSWORD, DATABASE_USER, HOST
+from app.database.tables.base import Base
 
 
-database_name = "test_equipment_manager"
-
-
-def create_database() -> None:
+class Database():
     '''
-    Метод для создания базы данных
-    '''
-    try:
-        with connect(
-            host=HOST,
-            user=DATABASE_USER,
-            password=DATABASE_PASSWORD) as connection:
-            create_db_query = f"CREATE DATABASE {database_name}"
-            with connection.cursor() as cursor:
-                cursor.execute(create_db_query)
-    except Error as e:
-        print(e)
-
-
-def new_engine() -> Engine:
-    '''
-    Создание движка SQLalchemy
+    База данных
     '''
 
-    # строка подключения
-    sqlite_database = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{HOST}/{database_name}"
-    # создаем движок SqlAlchemy
-    engine = create_engine(sqlite_database, echo=True)
+    def __init__(self) -> None:
 
-    return engine
+        self.database_name = "test_equipment_manager"
+        self.create_database()
+
+
+    def create_database(self) -> None:
+        '''
+        Метод для создания базы данных
+        '''
+        try:
+            with connect(
+                host=HOST,
+                user=DATABASE_USER,
+                password=DATABASE_PASSWORD) as connection:
+                create_db_query = f"CREATE DATABASE {self.database_name}"
+                with connection.cursor() as cursor:
+                    cursor.execute(create_db_query)
+        except Error as e:
+            print(e)
+
+
+    def new_engine(self) -> Engine:
+        '''
+        Создание движка SQLalchemy
+        '''
+
+        # строка подключения
+        mysql_database = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{HOST}/{self.database_name}"
+        # создаем движок SqlAlchemy
+        self.engine = create_engine(mysql_database, echo=True)
+
+        self.create_table()
+        return self.engine
+
+
+    def create_table(self) -> None:
+        '''
+        Создание таблиц
+        '''
+        # создаем таблицы
+        Base.metadata.create_all(bind=self.engine)
