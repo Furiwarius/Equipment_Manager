52690bdd021a8e2017bb076280489bb898041339
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-06-08 11:48:12 +0300

    feat: added function for creating SQLalchemy engine

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,12 @@
+from sqlalchemy import create_engine, Engine
 from mysql.connector import connect, Error
 from app.settings.settings import DATABASE_PASSWORD, DATABASE_USER, HOST
 
-def create_database():
+
+database_name = "test_equipment_manager"
+
+
+def create_database() -> None:
     '''
     Метод для создания базы данных
     '''
@@ -10,8 +15,21 @@ def create_database():
             host=HOST,
             user=DATABASE_USER,
             password=DATABASE_PASSWORD) as connection:
-            create_db_query = "CREATE DATABASE test_equipment_manager"
+            create_db_query = f"CREATE DATABASE {database_name}"
             with connection.cursor() as cursor:
                 cursor.execute(create_db_query)
     except Error as e:
         print(e)
+
+
+def new_engine() -> Engine:
+    '''
+    Создание движка SQLalchemy
+    '''
+
+    # строка подключения
+    sqlite_database = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{HOST}/{database_name}"
+    # создаем движок SqlAlchemy
+    engine = create_engine(sqlite_database, echo=True)
+
+    return engine
