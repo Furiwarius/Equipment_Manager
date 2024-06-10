3c9a9f1f8e28d9af5464b4d4748e76263aae46bd
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-06-10 11:59:44 +0300

    feat: methods for converting data into a table object have been added to the converter class

--- app/database/converter.py
+++ app/database/converter.py
@@ -30,6 +30,21 @@ class Converter():
             return self.__conversion_to_tool(obj)
     
 
+    def conversion_to_table(self, obj:Constr|Work|Tool|Stor) -> ConstrT|WorkT|ToolT|StorT:
+        '''
+        Конвертация из объекта с голыми
+        данными в объект Table
+        '''
+        if isinstance(obj, Constr):
+            return self.__conversion_to_constrT(obj)
+        elif isinstance(obj, Stor):
+            return self.__conversion_to_storT(obj)
+        elif isinstance(obj, Work):
+            return self.__conversion_to_workT(obj)
+        elif isinstance(obj, Tool):
+            return self.__conversion_to_toolT(obj)
+
+
     def __conversion_to_tool(self, tool:ToolT) -> Tool:
         '''
         Переводит данные из класса ToolTable в Tool
@@ -83,4 +98,61 @@ class Converter():
                         start_date=storage.start_date,
                         end_date=storage.end_date)
         
+        return result
+
+
+    def __conversion_to_constrT(self, constr:Constr) -> ConstrT:
+        '''
+        Переводит данные из класса Construction в ConstructionTable
+        '''
+
+        result = ConstrT(id=constr.id, name=constr.name,
+                        project=constr.project,
+                        address=constr.address,
+                        status=constr.status,
+                        start_date=constr.start_date,
+                        end_date=constr.end_date)
+        
+        return result
+    
+
+    def __conversion_to_storT(self, storage:Stor) -> StorT:
+        '''
+        Переводит данные из класса Storage в StorageTable
+        '''
+
+        result = StorT(id=storage.id, name=storage.name,
+                        address=storage.address,
+                        status=storage.status,
+                        start_date=storage.start_date,
+                        end_date=storage.end_date)
+        
+        return result
+
+
+    def __conversion_to_workT(self, worker:Work) -> WorkT:
+        '''
+        Переводит данные из класса Worker в WorkerTable
+        '''
+
+        result = WorkT(id=worker.id, name=worker.name,
+                      surname=worker.surname,
+                      phone_number=worker.phone_number,
+                      job_title=worker.job_title,
+                      start_work=worker.start_work,
+                      dismissal_work=worker.dismissal_work,
+                      status=worker.status)
+
+        return result
+
+
+    def __conversion_to_toolT(self, tool:Tool) -> ToolT:
+        '''
+        Переводит данные из класса Tool в ToolTable
+        '''
+
+        result = ToolT(id=tool.id, name=tool.name,
+                      status=tool.status, 
+                      factory_number=tool.factory_number)
+
         return result
