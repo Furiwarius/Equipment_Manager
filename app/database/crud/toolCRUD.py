b6fbdd18544d168568b8335543e1b14d0e1da469
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-06-10 14:38:27 +0300

    feat: a method for adding a tool is prescribed

--- app/database/crud/toolCRUD.py
+++ app/database/crud/toolCRUD.py
@@ -4,8 +4,9 @@ from app.entities.construction import Construction
 from app.entities.storage import Storage
 from app.database.crud.baseCRUD import BaseCRUD
 from app.database.tables.essence import ToolTable
-from app.database.converter import Converter
-
+from sqlalchemy.orm import Session
+from app.database.tables.summary import ToolsOnConstructions, ToolsOnStorage
+from datetime import datetime
 
 class ToolCRUD(BaseCRUD):
     '''
@@ -24,6 +25,28 @@ class ToolCRUD(BaseCRUD):
         Для добавления нового инструмента, нужно также
         указать объект или склад, где он будет хранится.
         '''
+        tool = self.coverter.conversion_to_table(tool)
+        with Session(autoflush=False, bind=self.engine) as db:
+
+            db.add(tool)     # добавляем в бд
+            db.commit()
+
+            if isinstance(where, Storage):
+                storage = self.coverter.conversion_to_table(where)
+                where = ToolsOnStorage(tool_id=tool.id,
+                                         storage_id=storage.id,
+                                         DT_start=datetime.now(),
+                                         DT_end=None)
+
+            else:
+                constr = self.coverter.conversion_to_table(where)
+                where = ToolsOnConstructions(tool_id=tool.id,
+                                         construction_id=constr.id,
+                                         DT_start=datetime.now(),
+                                         DT_end=None)
+            
+            db.add(where)
+            db.commit()     # сохраняем изменения
 
 
     def move_to_storage(self, tool:Tool, where:Storage) -> None:
