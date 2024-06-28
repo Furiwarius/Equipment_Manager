f6d21480986b7f729952e4e109268bf4a7cd8dee
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-06-28 14:16:26 +0300

    fix: the DatabaseLogger class has been improved
    Bugs with choosing the logger operating mode have been fixed. A decorator method has also been added to track incoming and outgoing data.

--- app/loggers/database_logger/db_logger.py
+++ app/loggers/database_logger/db_logger.py
@@ -22,23 +22,50 @@ class DatabaseLogger():
     отключен.
     '''
 
-    log_setting = 'app/settings/database_log.conf'
+    log_write = 'app/settings/database_log_write.conf'
+    log_print = 'app/settings/database_log_print.conf'
+    
 
+    def get_logger(self, mode:ModeLogger=ModeLogger.write):
+        '''
+        Возвращает логгер с нужным режимом работы
+        '''
 
-    def __init__(self, mode:ModeLogger=ModeLogger.write) -> None:
-        '''
-        При инициализации ключевым аргументом для
-        настройки логгера является режим работы (mode).
-        '''
-        
-        logging.config.fileConfig(fname=DatabaseLogger.log_setting)
-        
-        
         if mode is ModeLogger.write:
-            self.log = logging.getLogger('root')
+            self._setting_logger(self.log_write)
+            self.logger = logging.getLogger('root')
 
         elif mode is ModeLogger.print_:
-            self.log = logging.getLogger('print')
+            self._setting_logger(self.log_print)
+            self.logger = logging.getLogger('root')
 
         elif mode is ModeLogger.disable:
-            self.log = logging.NullHandler()
+            self.logger = logging.NullHandler()
+
+        return self.logger
+
+
+    def _setting_logger(self, setting:str) -> None:
+        '''
+        Чтение настроек для логгера базы данных
+        '''
+
+        logging.config.fileConfig(setting)
+
+    
+    def info(self, func):
+        '''
+        Выводит информацию о методе и послупающих в него данных
+        '''
+        def work_with_func(*args, **kwargs):
+            
+            if isinstance(self.logger, logging.NullHandler):
+                result = func(*args, **kwargs)
+            else:
+                self.logger.info(f"method: {func.__name__}; input date: {args} {kwargs}")
+                result = func(*args, **kwargs)
+                self.logger.info(f"method: {func.__name__}; output date: {result}")
+
+            return result
+
+        return work_with_func
