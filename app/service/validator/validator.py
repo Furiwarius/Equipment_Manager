d7a07f3d0ca5c3132e082faa7229421512d02fe8
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-07-03 12:55:57 +0300

    feat: methods are written in the DataValidator class

--- app/service/validator/validator.py
+++ app/service/validator/validator.py
@@ -3,6 +3,10 @@ from app.entities.storage import Storage
 from app.entities.tool import Tool
 from app.entities.worker import Worker
 from datetime import datetime
+from app.errors.service_error.validator_error import PresenceNumbers, ForbiddenSymbols
+from app.errors.service_error.validator_error import NonDisplayableSymbols, NotDatetime
+from app.errors.service_error.validator_error import DateMismatch, NotNumber, InvalidLength
+
 
 
 class DataValidator():
@@ -14,7 +18,7 @@ class DataValidator():
     prohibited = '''/|!?}{[]"'`~+=()*^%$#<>'''
 
 
-    def strings_with_number(self, string:str) -> None:
+    def strings_with_number(self, string:str, length=0) -> None:
         '''
         Проверка строковых значений
         с входящими в них цифрами
@@ -23,9 +27,22 @@ class DataValidator():
         или склада, номер проекта, номер договора,
         название инструмента, заводской номер инструмента
         '''
+
+        symbols = set(string)
+
+        if not symbols.isdisjoint(set(self.prohibited)):
+            raise ForbiddenSymbols
+
+        elif string.isspace():
+            raise NonDisplayableSymbols
+
+        # Если задана необходимая длина строки
+        elif length and len(string)>length:
+            raise InvalidLength
     
 
-    def only_strings(self, string:str) -> None:
+
+    def only_strings(self, string:str, length=0) -> None:
         '''
         Проверка строковых значений
 
@@ -33,24 +50,57 @@ class DataValidator():
         фамилия работника, должность работника
         '''
 
+        nummers = set(range(10))
+        symbols = set(string)
+
+        if not symbols.isdisjoint(nummers):
+            raise PresenceNumbers
+        
+        elif not symbols.isdisjoint(set(self.prohibited)):
+            raise ForbiddenSymbols
+        
+        elif string.isspace():
+            raise NonDisplayableSymbols
+
+        # Если задана необходимая длина строки
+        elif length and len(string)>length:
+            raise InvalidLength
+
+
 
     def start_date(self, date:datetime) -> None:
         '''
         Проверка даты начала работ
         '''
 
+        if not isinstance(date, datetime):
+            raise NotDatetime
+
+
 
     def end_date(self, start_date:datetime, end_date:datetime) -> None:
         '''
         Проверка даты окончания
         '''
+
+        if not isinstance(start_date, datetime) or not isinstance(end_date, datetime):
+            raise NotDatetime
+        
+        elif end_date<start_date:
+            raise DateMismatch
     
+
 
     def phone_number(self, phone:str) -> None:
         '''
         Проверка номера телефона
         '''
-    
+
+        if not phone.isdigit():
+            raise NotNumber
+
+        elif len(phone) != 11:
+            raise InvalidLength
 
         
 
