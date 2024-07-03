a5bb304ac186ef78197c51a33ef75fb606b4e00f
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-07-03 23:11:47 +0300

    fix: tests were written and bugs identified by them were fixed

--- app/tests/service_tests/test_logic.py
+++ app/tests/service_tests/test_logic.py
@@ -11,6 +11,8 @@ from app.database.crud.storageCRUD import StorageCRUD
 from app.database.crud.constructionCRUD import ConstructionCRUD
 from app.database.crud.workerCRUD import WorkerCRUD
 from app.errors.service_error.validator_error import BaseValidatorException
+from app.errors.service_error.construction_error import ResponsibleAbsent
+from app.errors.service_error.worker_error import WorkerDoesntWork
 
 
 class TestBusinessLogic():
@@ -49,44 +51,116 @@ class TestBusinessLogic():
         Тестирование исключений выпадающих 
         при добавлении склада с неправильными атрибутами
         '''
+
         with pytest.raises(BaseValidatorException):
             broken_storage = self.generator.storage_generator()
             broken_storage.name = "   "
-            storage_manager = StorM(broken_storage)
 
-
-    def test_add_tool(self):
-        '''
-        Тестирование метода по добавлению инструмента
-        '''
+            StorM(broken_storage)
         
+        assert self.stor_crud.get_all()[-1].name != broken_storage.name     
+
+
+
+    def test_add_tool_in_storage(self):
+        '''
+        Тестирование метода по добавлению 
+        инструмента на склад
+        '''
+
+        new_tool = self.generator.tool_generator()  
+        
+        storage = self.stor_crud.get_all()[-1]
+        stor_manager = StorM(storage)
+        
+        stor_manager.add_tool(new_tool)      
+
+        assert self.tool_crud.get_all()[-1].id in self.stor_crud.get_tools(storage) 
+
+
+
+    def test_add_construction(self):
+        '''
+        Тестирование метода по добавлению объекта строительства
+        '''
+
+        new_constr = self.generator.constr_generator()
+        constr_manager = ConstrM(new_constr)
+
+        assert self.constr_crud.get_all()[-1].id is constr_manager.constr.id
+
+
+
+    def test_add_tool_in_construction_without_responsible(self):
+        '''
+        Тестирование метода по добавлению 
+        инструмента на объект строительства
+        без ответственного лица
+        '''
+
+        with pytest.raises(ResponsibleAbsent):
+            
+            new_tool = self.generator.tool_generator()
+            constr = self.constr_crud.get_all()[-1]
+            
+            constr_m = ConstrM(constr)
+            constr_m.add_tool(new_tool)
+
 
     
     def test_add_worker(self):
         '''
         Тестирование метода по добавлению работника
         '''
-        
-    
 
-    def test_add_construction(self):
-        '''
-        Тестирование метода по добавлению объекта строительства
-        '''
-        
+        new_worker = self.generator.worker_generator()
+        worker_manager = WorkM(new_worker)
+
+        assert self.work_crud.get_all()[-1].id is worker_manager.worker.id
+
+
 
     def test_appointment_healthy_responsible(self):
         '''
         Тестирование метода по назначению ответственного лица на объект (Работник здоров)
         '''
+
+        worker = self.work_crud.get_all()[-1]
+        assert worker.status
+
+        constr = self.constr_crud.get_all()[-1]
+        assert not self.constr_crud.get_responsible(constr)
+
+        constr_m = ConstrM(constr)
+        constr_m.appointment_responsible(worker)
         
-    
+        assert self.work_crud.is_brigadir(worker).id is constr.id
+        assert self.constr_crud.get_responsible(constr).id is worker.id
+
+
 
     def test_appointment_sick_responsible(self):
         '''
         Тестирование метода по назначению ответственного лица на объект (Работник болен)
         '''
+
+        constr = self.constr_crud.get_all()[-1]
+        assert self.constr_crud.get_responsible(constr)
+
+        worker = self.generator.worker_generator()
+        worker_m = WorkM(worker)
+        worker_m.get_sick() 
         
+        sick_worker = self.work_crud.get_by_id(id=worker_m.worker.id)
+        assert not sick_worker.status
+
+        constr_m = ConstrM(constr)
+
+        with pytest.raises(WorkerDoesntWork):
+            constr_m.appointment_responsible(sick_worker)
+        
+        assert self.constr_crud.get_responsible(constr_m.constr).id!=sick_worker.id
+
 
     
     def test_appointment_engaged_responsible(self):
@@ -95,7 +169,19 @@ class TestBusinessLogic():
         лица с уже имеющимся объектом и инструментами на нем
         '''
         
-    
+        old_constr = self.constr_crud.get_all()[-1]
+        responsible = self.constr_crud.get_responsible(old_constr)
+        assert responsible
+
+        new_constr = self.generator.constr_generator()
+        constr_m = ConstrM(new_constr)
+        constr_m.appointment_responsible(responsible)
+
+        assert self.constr_crud.get_responsible(constr_m.constr).id is responsible.id
+        assert not self.constr_crud.get_responsible(old_constr)
+        assert self.work_crud.is_brigadir(responsible).id is constr_m.constr.id
+
+
 
     def test_move_tool(self):
         '''
