1fdbaf0660be80308b1fe91ad72e124940121ccf
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-07-23 22:11:54 +0200

    feat: added exception for crud class

---
+++ app/errors/database_error/database_error.py
@@ -0,0 +1,24 @@
+
+
+class ThisIsSuperAdmin(Exception):
+    '''
+    Вызывается при попытке сменить роль
+    super_admin на другую
+    '''
+
+    def __init__(self):
+        
+        message = "Попытка поменять super_admin в фирме"
+        super().__init__(message)
+
+
+
+class CannotGiveSuperadmin(Exception):
+    '''
+    Вызывается при попытке назначить роль super_admin
+    '''
+
+    def __init__(self):
+        
+        message = "Попытка назначить роль super_admin"
+        super().__init__(message)
