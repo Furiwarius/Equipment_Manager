565b9ac3af1e42c05be99aade6dfe13605087d17
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-07-25 19:16:54 +0200

    fix: the AccountManager class has been redesigned

--- app/service/user_account/account.py
+++ app/service/user_account/account.py
@@ -10,77 +10,125 @@ from app.entities.account import Account
 class AccountManager():
     
 
-    def __init__(self, login:str, password:str, email:str=None) -> None:
+    def __init__(self, account:Account, send_code:bool=True) -> None:
+        '''
+        При инициализации передается экземпляр Accaunt
+        в нем обязательно должны быть логин и пароль,
+        если этот аккаунт существует. Если же это новый
+        аккаунт, то необходимо так же передать почту, для
+        проверки уникальности.
+
+        Атрибут send_code=True значит, что при создании
+        нового аккаунта будет автоматически отправляться
+        код для подтверждения аккаунта. Если False, то 
+        аккаунт можно будет подтверить позже.
+        '''
 
         self.account_crud = AccountCRUD()
 
-        if email:
-            self._create(login, password, email)
+        # Сюда поступают чистые данные из слоя Api
+        # поэтому необходимо перед работой перевести их в hash
+        account.login = to_hash(account.login)
+        account.password = to_hash(account.password)
+
+        if account.id is None:
+            self._new_account(account, send_code)
+
         else:
-            self.account = self.is_correct(login, password)
+            self._exist_account(account)
         
+    
 
-
-    def is_correct(self, login:str, password:str) -> Account:
+    def _new_account(self, new_account:Account, send_code:bool) -> None:
         '''
-        Сравнение данных
+        Операции для создания нового аккаунта
         '''
-        acc = self.account_crud.get_account_by_login(to_hash(login))
-        
-        if acc is None:
-            # Если логина нет в бд
-            raise IncorrectLogin
-        
-        elif acc.password is to_hash(password):
-            # Если пароль не совпадает с тем, который сохранен в бд
-            raise IncorrectPassword
+        self._check_uniqueness(new_account)
+        self._create(new_account)
 
-        return acc
+        if send_code:
+            # Отправка письма с проверочный кодом на почту
+            self.verification(self.account.email)
     
 
 
-    def change_password(self, login:str, new_password:str) -> None:
+    def _exist_account(self, account:Account) -> None:
+        '''
+        Операции для начала работы с существуюим аккаунтом
+        '''
+        # Проверка на наличие аккаунта
+        self._check_exist(account)
+        # Проверка на коректность введенных данных
+        self._is_correct(account)
+
+        # Так как могут передаваться не все данные,
+        # то вызывается этот метод, чтобы подтянуть их из БД
+        self.account = self.account_crud.get_by_id(id=account.id)
+
+
+
+    def _check_uniqueness(self, account:Account) -> None:
+        '''
+        Проверка уникальности передаваемых значений
+        '''
+        try:
+            self.account_crud.check_data(login=account.login,
+                                     email=account.email)
+        except Exception as err:
+            raise err
+
+    
+
+    def _check_exist(self, account:Account) -> None:
+        '''
+        Проверка наличия аккаунта
+        '''
+        bd_acc = self.account_crud.get_by_id(account.id)
+
+        if account.login!=bd_acc.login:
+            raise IncorrectLogin
+        
+
+
+    def _is_correct(self, account:Account) -> None:
+        '''
+        Сравнение паролей из бд и переданного
+        '''
+        acc = self.account_crud.get_account_by_login(account.login)
+    
+        if acc.password is account.password:
+            # Если пароль не совпадает с тем, который сохранен в бд
+            raise IncorrectPassword
+    
+
+
+    def change_password(self, new_password:str) -> None:
         '''
         Измненение пароля
         '''
 
 
 
-    def _create(self, login:str, password:str, email:str) -> None:
+    def _create(self, account:Account) -> None:
         '''
         Создание аккаунта
         '''
-
-        if self.account_crud.get_account_by_login(to_hash(login)):
-            # Если логин есть в БД
-            raise LoginExists
-        elif self.account_crud.get_account_by_email(email):
-            # Если аккаунт с такой почтой уже есть
-            raise EmailExists
         
-        new_account = Account(login=to_hash(login), 
-                              password=to_hash(password),
-                              email=email,
-                              timezone=get_localzone())
-        
-        self.account = self.account_crud.add(new_account)
-        
-        # Отправка письма с проверочный кодом на почту
-        self.verification(email)
+        self.account = self.account_crud.add(account)
 
 
 
-    def verification(self, email:str) -> None:
+    def verification(self) -> None:
         '''
         Отправка проверочного кода
         '''
 
-        self.code = SenderCode(email)
+        self.code = SenderCode(self.account.email)
         self.code.send_code()
     
 
     
-    def confirmation(self, code:int) -> None:
+    def _confirmation(self, code:int) -> None:
         '''
         Подтверждение аккаунта
         '''
