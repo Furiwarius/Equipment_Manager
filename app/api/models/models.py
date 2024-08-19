d7b7e05fadd40ff0edfa30141ad58545bb61e872
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-08-19 16:06:56 +0300

    fix: pydantic models have been improved

--- app/api/models/models.py
+++ app/api/models/models.py
@@ -1,35 +1,18 @@
 import re
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, EmailStr, Field
 
 
 class NewUser(BaseModel):
-    login: str
-    email: str
-    password: str
+    login: str = Field(..., min_length=8)
+    email: EmailStr
+    password: str = Field(..., min_length=8, max_length=64)
     timezone: str
-
-
-    @field_validator("email")
-    @classmethod
-    def validate_email(cls, value):
-        if not bool(re.fullmatch(r'[\w.-]+@[\w-]+\.[\w.]+', value)):
-            raise ValueError("Email is invalid")
-        return value 
-
-
-    @field_validator("password")
-    @classmethod
-    def validate_password(cls, value):
-        password_length = len(value)
-        if password_length < 8 or password_length > 64:
-            raise ValueError("The password must be between 8 and 64 characters long")
-        return value
 
 
 
 class User(BaseModel):
-    login:str
-    password:str
+    login: str = Field(..., min_length=8)
+    password: str = Field(..., min_length=8, max_length=64)
 
 
 
@@ -45,7 +28,7 @@ class Code(BaseModel):
 
 
 class NewFirm(BaseModel):
-    name:str
+    name: str = Field(..., min_length=6, max_length=64)
 
 
 class NewConstruction(BaseModel):
