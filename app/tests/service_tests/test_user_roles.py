a7cd8fd0c5e332c7ef8cd1c81dccfb5e9502362c
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-08-20 16:39:39 +0300

    store-and-forward

--- app/tests/service_tests/test_user_roles.py
+++ app/tests/service_tests/test_user_roles.py
@@ -1,8 +1,8 @@
 from app.tests.fake_data import DataGenerator
-from app.database.crud.accountCRUD import AccountCRUD
+from app.database.crud.accountCRUD import AccountCRUD, Account
 from app.database.crud.firmCRUD import FirmCRUD, Roles
 from app.service.user_account.account import AccountManager
-from app.service.firm.firm import FirmManager
+from app.service.firm.firm import FirmManager, Firm
 from app.errors.service_error.account_error import (IncorrectLogin, 
                                                     IncorrectPassword, 
                                                     LoginExists, 
@@ -25,33 +25,31 @@ class TestUserRoles():
 
 
 
-    def test_add_new_account(self):
+    def test_add_new_account(self, account:Account):
         '''
         Тестирование метода по добавлению нового аккаунта
         '''
-        new_account = self.generator.account_generate()
 
-        account_m = AccountManager(new_account, send_code=False, new=True)
+        account_m = AccountManager(account, send_code=False, new=True)
 
         assert account_m.account.id
 
     
     
-    def test_add_exist_account(self):
+    def test_add_exist_account(self, account:Account, acc_crud:AccountCRUD):
         '''
         Тестирование метода по инициализации существующего аккаунта
         '''
-        new_account = self.generator.account_generate()
 
-        AccountManager(copy(new_account), send_code=False, new=True)
+        AccountManager(copy(account), send_code=False, new=True)
 
-        account_m = AccountManager(copy(new_account), send_code=False)
+        account_m = AccountManager(copy(account), send_code=False)
 
-        assert self.acc_crud.get_last_one().id is account_m.account.id
-
+        assert acc_crud.get_last_one().id is account_m.account.id
 
 
-    def test_account_exception(self):
+    @pytest.mark.skip()
+    def test_account_exception(self, account:Account, acc_crud:AccountCRUD):
         '''
         Тестирование работы исключений при работе с AccuntManager
         '''
@@ -80,34 +78,33 @@ class TestUserRoles():
 
 
 
-    def test_add_firm(self):
+    def test_add_firm(self, firm:Firm, acc_crud:AccountCRUD, firm_crud:FirmCRUD):
         '''
         Тестирование метода по добавлению фирмы
         '''
-        new_firm = self.generator.firm_generate()
-        account = self.acc_crud.get_last_one()
 
-        firm_m = FirmManager(firm=new_firm, 
+        account = acc_crud.get_last_one()
+
+        firm_m = FirmManager(firm=firm, 
                              account_id=account.id)
         assert firm_m.firm.id
 
-        firms = self.firm_crud.get_all(account_id=account.id)
+        firms = firm_crud.get_all(account_id=account.id)
         assert firm_m.firm.id in firms
 
     
 
-    def test_give_role(self):
+    def test_give_role(self, account:Account, firm_crud:FirmCRUD):
         '''
         Тестирование метода по выдаче роли аккаунту
         '''
-        account = self.generator.account_generate()
         acc_m = AccountManager(account, send_code=False, new=True)
 
-        firm = self.firm_crud.get_last_one()
+        firm = firm_crud.get_last_one()
         firm_m = FirmManager(firm)
 
         firm_m.give_role(account_id=acc_m.account.id,
                          role=Roles.admin.name)
         
-        assert self.firm_crud.get_role(account_id=acc_m.account.id,
+        assert firm_crud.get_role(account_id=acc_m.account.id,
                                        firm_id=firm_m.firm.id) == Roles.admin.name
