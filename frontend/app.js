520c9a1d1ab64f76c5ee147d70746a8a31f4228a
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-08-27 14:43:51 +0300

    store-and-forward

--- frontend/app.js
+++ frontend/app.js
@@ -2,24 +2,46 @@ const http = require("http");
 const fs = require("fs");
    
 http.createServer(function(request, response){
-       
+    
     let filePath = "public/index.html";
-    if(request.url !== "/"){
-        // получаем путь после слеша
-        filePath = request.url.substring(1);
-    }
-    fs.readFile(filePath, function(error, data){
-               
-        if(error){
+    
+    switch (request.url) {
+
+        case "/":
+
+            fs.readFile(filePath, function(error, data){
+                if(error){
                    
+                    response.statusCode = 404;
+                    response.end("Resourse not found!");
+                }   
+                else{
+                    response.end(data);
+                }
+            });
+            break;
+        
+        
+        case "/login":
+            console.log(response.data)
+            break;
+
+        
+        case "/registr":
+            break;
+        
+        case "/confirmation_code":
+            break;
+
+        case "/check_confirmation_code":
+            break;
+
+        default:
             response.statusCode = 404;
             response.end("Resourse not found!");
-        }   
-        else{
-            response.end(data);
-        }
-    });
-     
+            break;
+    }
+
 }).listen(3000, function(){
     console.log("Server started at http://localhost:3000");
 });
